ef362bde8e074fa4b7f38b654c53b266e9c41a3f
BlackMG27 <oike279@gmail.com>
2021-12-22 13:18:05 -0500

    having a'TypeError: failed to fetch' issue

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,30 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import React, {useState} from 'react';
-import {Box, Container, Text, Wrap, WrapItem} from '@chakra-ui/react';
-import { getCuratedPhotos } from '../../src/lib/api';
+import {Box, Container, Text, Wrap, WrapItem, Input, IconButton, InputRightElement, InputGroup} from '@chakra-ui/react';
+import { SearchIcon } from '@chakra-ui/icons';
+import { getCuratedPhotos, getQueryPhotos } from '../lib/api';
 
 export default function Home({data}) {
   const [photos, setPhotos] = useState(data);
+  const [query, setQuery] = useState('');
+  const handleSubmit = async (e) => {
+    await e.preventDefault();
+    const res = await getQueryPhotos(query);
+    await setPhotos(res)
+    await setQuery("");
+  }
+
   return (
     <div>
       <Head>
         <title>Next.js Image Gallery</title>
         <link rel="icon" href="/favicon.ico"/>
       </Head>
-      <Box overflow="hidden" bg="gray.50" minH="100vh" minW="100%">
-        <Container>
+      <Box overflow="hidden" bg="gray.200" minH="100vh" >
+        <Container minW="100%" maxW="100vw" pt="2rem">
           <Text
             color="purple.800"
-            maxW="100vw"
             fontWeight="bold"
             mb="1.5rem"
             textAlign="center"
@@ -24,6 +32,30 @@ export default function Home({data}) {
           >
             Next.Js Image Gallery
           </Text>
+          <form onSubmit={handleSubmit}>
+            <InputGroup pb="2rem" width="90%" maxW="70ch" margin="0 auto">
+              <Input 
+                placeholder="Search for Books" 
+                variant="ghost"
+                value={query}
+                onChange={e => setQuery(e.target.value)}
+                />
+              <InputRightElement
+              children={
+                <IconButton
+                  aria-label="Search"
+                  onClick={handleSubmit}
+                  px="30px"
+                  icon={<SearchIcon/>}
+                  bg="purple.800"
+                  color="gray.100"
+                  _hover={{backgroundColor: "green.400"}}
+                  _active={{backgroundColor: "green.700"}}
+                />
+                }
+              />
+            </InputGroup>
+          </form>
           <Wrap px=".5rem" spacing={4} justify="center">
           {
             photos.map((photo) => (
@@ -31,10 +63,13 @@ export default function Home({data}) {
                 key={photo.id}
                 boxShadow="base"
                 rounded="15px"
+                cursor="pointer"
                 overflow="hidden"
                 bg="white"
                 lineHeight="0"
-                _hover={{boxShadow: "dark-lg"}}
+                _hover={{
+                  boxShadow: "dark-lg",
+                }}
               >
                 <Image src={photo.src.portrait} height={600} width={400} alt={photo.alt}/>
               </WrapItem>
