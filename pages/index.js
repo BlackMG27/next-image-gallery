65104592b8c47ff34d294c1c32b4bfb68587ef47
BlackMG27 <oike279@gmail.com>
2021-12-22 10:36:37 -0500

    fix the gallery grid issue

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,22 @@
 import Head from 'next/head'
-import {Box, Container, Text} from '@chakra-ui/react';
+import Image from 'next/image'
+import React, {useState} from 'react';
+import {Box, Container, Text, Wrap, WrapItem} from '@chakra-ui/react';
 import { getCuratedPhotos } from '../../src/lib/api';
 
 export default function Home({data}) {
+  const [photos, setPhotos] = useState(data);
   return (
     <div>
       <Head>
         <title>Next.js Image Gallery</title>
         <link rel="icon" href="/favicon.ico"/>
       </Head>
-      <Box overflow="hidden" bg="gray.50" minH="100vh">
+      <Box overflow="hidden" bg="gray.50" minH="100vh" minW="100%">
         <Container>
           <Text
             color="purple.800"
+            maxW="100vw"
             fontWeight="bold"
             mb="1.5rem"
             textAlign="center"
@@ -20,6 +24,23 @@ export default function Home({data}) {
           >
             Next.Js Image Gallery
           </Text>
+          <Wrap px=".5rem" spacing={4} justify="center">
+          {
+            photos.map((photo) => (
+              <WrapItem
+                key={photo.id}
+                boxShadow="base"
+                rounded="15px"
+                overflow="hidden"
+                bg="white"
+                lineHeight="0"
+                _hover={{boxShadow: "dark-lg"}}
+              >
+                <Image src={photo.src.portrait} height={600} width={400} alt={photo.alt}/>
+              </WrapItem>
+            ))
+          }
+          </Wrap>
         </Container>
       </Box>
     </div>
